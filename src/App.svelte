a7827d2893ad092b2d672505d3897424296ef62b
ruelickejustin <ruelicke.justin@outlook.de>
2025-11-06 22:47:26 +0100

    Create src/App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  let count = 0;
+
+  function increment() {
+    count += 1;
+  }
+</script>
+
+<main>
+  <h1>Hello Svelte!</h1>
+  <button on:click={increment}>Count: {count}</button>
+</main>
