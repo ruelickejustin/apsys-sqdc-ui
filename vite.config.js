50d4ffff39b059309adf891fd155043bace6981e
ruelickejustin <ruelicke.justin@outlook.de>
2025-11-06 23:17:47 +0100

    Change base path in vite.config.js
    Updated base path for deployment in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
-export default defineConfig({ plugins: [svelte()], build: { outDir: 'dist' }, base: './' })
+
+export default defineConfig({
+  plugins: [svelte()],
+  base: '/apsys-sqdc-ui/',   // <- genau so, mit deinem Repo-Namen
+  build: { outDir: 'dist' }
+})
