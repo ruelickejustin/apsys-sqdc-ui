5b914ce5add06190736a2c27544acac2b428471a
ruelickejustin <ruelicke.justin@outlook.de>
2025-11-07 08:09:52 +0100

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [svelte()],
   base: '/apsys-sqdc-ui/',
-  build: { outDir: 'dist' }
+  plugins: [svelte()]
 })
